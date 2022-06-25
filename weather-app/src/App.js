8f93761b4461f402e38521808da1fbfd591366d0
MateuszChmiest <mateusz_269@icloud.com>
2022-06-25 17:06:02 +0200

    fetch api

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,57 +1,125 @@
-import React from "react";
+import React, { useState } from "react";
 import { BsFillSunFill } from "react-icons/bs";
-import { FaSearch } from "react-icons/fa"
+import { FaSearch } from "react-icons/fa";
 
 function App() {
+	const [citySelect, setCitySelect] = useState(false);
+	const [cityInput, setCityInput] = useState("");
+
+	const [temp, setTemp] = useState("27");
+  const [condition, setCondition] = useState("Sunny");
+  const [cityName, setCityName] = useState("Warsaw");
+  const [date, setDate] = useState("12,02,2022");
+  const [time, setTime] = useState("00:00");
+  const [iconID, setIconID] = useState("")
+
+	const cities = ["Berlin", "Warsaw", "New York", "Paris"];
+
+	const searchWeather = (e) => {
+		if (cityInput.length === 0) {
+			alert("Please type in a city name");
+		} else {
+		  fetchWeatherData();
+			setCityInput("");
+			// setCitySelect(true);
+		}
+		e.preventDefault();
+	};
+
+	const fetchWeatherData = () => {
+		fetch(
+			`http://api.weatherapi.com/v1/current.json?key=59b81ff816324b048da132739222506&q=${cityInput}`
+		)
+			.then((resp) => resp.json())
+			.then((data) => {
+				console.log(data);
+
+				setTemp(data.current.temp_c);
+        setCondition(data.current.condition.text);
+        setCityName(data.location.name);
+
+        const date = data.location.localtime;
+        const y = parseInt(date.substr(0,4));
+        const m = parseInt(date.substr(5,2));
+        const d = parseInt(date.substr(8,2));
+        const time = date.substr(11);
+
+        setDate(`${d}, ${m}, ${y}`);
+        setTime(time);
+
+        const iconID = data.current.condition.icon.substr("//cdn.weatherapi.com/weather/64x64/".length);
+        setIconID(`/weather/${iconID}`)
+			});
+	};
+
 	return (
-		<section className='App'>
+		<section
+			className='App'
+			style={citySelect ? { opacity: 0 } : { opacity: 1 }}>
 			<div className='App__container'>
 				<h3 className='App__brand'>The Weather</h3>
-				<div className="App__parameters">
-					<h1 className='App__temp'>16&#176;</h1>
+				<div className='App__parameters'>
+					<h1 className='App__temp'>{temp}&#176;</h1>
 					<div className='App__citytime'>
-						<h1 className='App__name'>London</h1>
+						<h1 className='App__name'>{cityName}</h1>
 						<small>
-							<span className='App__time'>06:09 -</span>
-							 <span className='App__date'> Monday, Sep 19</span>
+							<span className='App__time'>{time} -</span>
+							<span className='App__date'> {date}</span>
 						</small>
 					</div>
 					<div className='App__weather'>
 						<div className='App__icon'>
-							<BsFillSunFill />
+							<img src={iconID}/>
 						</div>
-						<span className='App__condition'>Cloudy</span>
+						<span className='App__condition'>{condition}</span>
 					</div>
 				</div>
 			</div>
 
-      <div className="App__panel">
-        <form id="locationInput">
-          <input type="text" className="App__search" placeholder="Search Location..."/>
-          <button type="submit" className="App__btn"><FaSearch/></button>
-        </form>
-        <ul className="App__cities">
-          <li className="App__city">New York</li>
-          <li className="App__city">California</li>
-          <li className="App__city">Paris</li>
-          <li className="App__city">Tokyo</li>
-        </ul>
-        <ul className="App__details">
-          <h4>Weather Details</h4>
-          <li>
-            <span>Cloudy</span>
-            <span className="App__cloud">89%</span>
-          </li>
-          <li>
-            <span>Humidity</span>
-            <span className="App__humidity">64%</span>
-          </li>
-          <li>
-            <span>Wind</span>
-            <span className="App__wind">8km/h</span>
-          </li>
-        </ul>
-      </div>
+			<div className='App__panel'>
+				<form id='locationInput' onSubmit={searchWeather}>
+					<input
+						type='text'
+						className='App__search'
+						placeholder='Search Location...'
+						value={cityInput}
+						onChange={(e) => setCityInput(e.target.value)}
+					/>
+					<button type='submit' className='App__btn'>
+						<FaSearch />
+					</button>
+				</form>
+				<ul className='App__cities'>
+        <h4>Popular Cities</h4>
+					{cities.map((city, index) => (
+						<li
+							className='App__city'
+							key={index}
+							onClick={(e) => {
+                setCityInput(e.target.innerHTML);
+                fetchWeatherData();
+              }			
+							}>
+							{city}
+						</li>
+					))}
+				</ul>
+				<ul className='App__details'>
+					<h4>Weather Details</h4>
+					<li>
+						<span>Cloudy</span>
+						<span className='App__cloud'>89%</span>
+					</li>
+					<li>
+						<span>Humidity</span>
+						<span className='App__humidity'>64%</span>
+					</li>
+					<li>
+						<span>Wind</span>
+						<span className='App__wind'>8km/h</span>
+					</li>
+				</ul>
+			</div>
 		</section>
 	);
 }
